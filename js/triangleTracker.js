825ae40539aa6bd045acdde5d770eb32c257b92b
Kameron Ross Johnson & Alister Fenix <student@epicodus.com>
2015-02-17 14:44:16 -0800

    add website functionality

--- js/triangleTracker.js
+++ js/triangleTracker.js
@@ -2,45 +2,43 @@ $(document).ready(function() {
     $("form#new-triangle").submit(function(event) {
       event.preventDefault();
 
-    var inputtedSide1 = $("input#side1").val();
-    var inputtedSide2 = $("input#side2").val();
-    var inputtedSide3 = $("input#side3").val();
+    var inputtedSide1 = parseInt($("input#side1").val());
+    var inputtedSide2 = parseInt($("input#side2").val());
+    var inputtedSide3 = parseInt($("input#side3").val());
 
     var Triangle = {
-        side1: inputtedSide1;
-        side2: inputtedSide2;
-        side3: inputtedSide3;
+        side1: inputtedSide1,
+        side2: inputtedSide2,
+        side3: inputtedSide3,
         type: function() {
-          if (this.side1 + this.side2 < this.side3) || (this.side2 + this.side3 < this.side1) || (this.side1 + this.side3 < this.side2) {
-                return "Not a triangle";
+            if (this.side1 + this.side2 < this.side3 || this.side1 + this.side3 < this.side2 || this.side2 + this.side3 < this.side1) {
+              return "Not a triangle";
             } else if (this.side1 === this.side2 && this.side2 === this.side3) {
-                return "Equilateral";
+              return "Equilateral";
             } else if (this.side1 === this.side2 || this.side1 === this.side3 || this.side2 === this.side3) {
-                return "Isosceles";
+              return "Isosceles";
             } else {
-                return "Scalene";
+              return "Scalene";
             }
           }
     };
 
-
-
     if (Triangle.type() === "Equilateral") {
-      $("ul#equilateral").append("<li><span class='equilateral'>"Triangle.side1 + ', ' + Triangle.side2+ ', ' + Triangle.side3"</span></li>" );
+      $("ul#equilateral").append("<li>" + Triangle.side1 +
+      ', ' + Triangle.side2 + ', ' + Triangle.side3 + "</li>" );
     } else if (Triangle.type() === "Isosceles") {
-      $("ul#isosceles").append("<li><span class='isosceles'>"Triangle.side1 + ', ' + Triangle.side2+ ', ' + Triangle.side3"</span></li>");
+      $("ul#isosceles").append("<li>" + Triangle.side1 +
+      ', ' + Triangle.side2 + ', ' + Triangle.side3 + "</li>" );
     } else if (Triangle.type() === "Scalene") {
-      $("ul#scalene").append("<li><span class='scalene'>"Triangle.side1 + ', ' + Triangle.side2+ ', ' + Triangle.side3"</span></li>");
-    } else (Triangle.type() === "Not a triangle") {
+      $("ul#scalene").append("<li>" + Triangle.side1 +
+      ', ' + Triangle.side2 + ', ' + Triangle.side3 + "</li>" );
+    } else {
       alert("That is not a triangle. Please try again.");
+    }
 
     $("input#side1").val("");
     $("input#side2").val("");
     $("input#side3").val("");
-
-    $("#equilateral-id").show();
-    $("#isosceles-id").show();
-    $("#scalene-id").show();
 
   });
 });
